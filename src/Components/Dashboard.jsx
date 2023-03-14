b19488169594ada45d4add3c83bab738c266bd36
Thirumurugan <muruganraj1705@gmail.com>
2023-03-14 21:08:26 +0530

    dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,87 +1,7 @@
 import React, { PureComponent } from "react";
 import { useContext } from "react";
 import quesnContext from "../ContextPage/Context";
-import { PieChart, Pie, Tooltip, Cell,ResponsiveContainer } from "recharts";
-
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
-
-const renderActiveShape = (props) => {
-  const RADIAN = Math.PI / 180;
-  const {
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    startAngle,
-    endAngle,
-    fill,
-    payload,
-    percent,
-    value,
-  } = props;
-  const sin = Math.sin(-RADIAN * midAngle);
-  const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  const ey = my;
-  const textAnchor = cos >= 0 ? "start" : "end";
-
-  return (
-    <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-        {payload.name}
-      </text>
-      <Sector
-        cx={cx}
-        cy={cy}
-        innerRadius={innerRadius}
-        outerRadius={outerRadius}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        fill={fill}
-      />
-      <Sector
-        cx={cx}
-        cy={cy}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        innerRadius={outerRadius + 6}
-        outerRadius={outerRadius + 10}
-        fill={fill}
-      />
-      <path
-        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-        stroke={fill}
-        fill="none"
-      />
-      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        textAnchor={textAnchor}
-        fill="#333"
-      >{`PV ${value}`}</text>
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        dy={18}
-        textAnchor={textAnchor}
-        fill="#999"
-      >
-        {`(Rate ${(percent * 100).toFixed(2)}%)`}
-      </text>
-    </g>
-  );
-};
+import { PieChart, Pie, Tooltip, Cell, ResponsiveContainer } from "recharts";
 
 function DashBoard() {
   let value = useContext(quesnContext);
@@ -98,25 +18,39 @@ function DashBoard() {
 
   //For Graph
 
-  let demoUrl =
-    "https://codesandbox.io/s/pie-chart-with-customized-active-shape-y93si";
+  const data = [
+    { name: "Group A", value: changer.correct },
+    { name: "Group B", value: changer.tot * 10 - changer.correct },
+  ];
 
-  state = {
-    activeIndex: 0,
-  };
+  let names=["a","b"]
 
-  onPieEnter = (_, index) => {
-    this.setState({
-      activeIndex: index,
-    });
+  const COLORS = ["#00C49F", "#FF8042"];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
   };
 
   return (
     <React.StrictMode>
-      <div className="h-screen w-screen bg-white flex justify-center items-center w-screen dashboard">
-        <div className="w-11/12 h-full bg-gray-200 rounded-xl ">
+      <div className="h-screen w-screen bg-white flex justify-center item-center w-screen dashboard">
+        <div className="w-11/12 h-full bg-blue-300 rounded-xl ">
           <div className="exit mr-5 mt-2  float-right">
-            <p className="btn text-white bg-red-400 px-2" onClick={closerFunc}>
+            <p
+              className="btn text-white bg-red-400 px-2 cursor-pointer"
+              onClick={closerFunc}
+            >
               X
             </p>
           </div>
@@ -174,23 +108,29 @@ function DashBoard() {
               </div>
             </div>
           </div>
-          <div className="">
-            <ResponsiveContainer width="100%" height="100%">
-              <PieChart width={400} height={400}>
-                <Pie
-                  activeIndex={this.state.activeIndex}
-                  activeShape={renderActiveShape}
-                  data={data}
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={60}
-                  outerRadius={80}
-                  fill="#8884d8"
-                  dataKey="value"
-                  onMouseEnter={this.onPieEnter}
-                />
-              </PieChart>
-            </ResponsiveContainer>
+          <div className="flex justify-center ">
+            <PieChart width={400} height={400}>
+              <Pie
+                dataKey="value"
+                startAngle={180}
+                endAngle={0}
+                data={data}
+                cx="50%"
+                cy="50%"
+                outerRadius={150}
+                fill="#8884d8"
+                labelLine={false}
+                label={renderCustomizedLabel}
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                    // key={names[index]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
           </div>
         </div>
       </div>
