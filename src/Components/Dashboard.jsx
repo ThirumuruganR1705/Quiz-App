2840ae332f6b00e4c94460c1551cb36be0d77375
Thirumurugan <muruganraj1705@gmail.com>
2023-03-15 07:47:18 +0530

    dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -23,7 +23,7 @@ function DashBoard() {
     { name: "Group B", value: changer.tot * 10 - changer.correct },
   ];
 
-  let names=["a","b"]
+  let names = ["a", "b"];
 
   const COLORS = ["#00C49F", "#FF8042"];
 
@@ -44,7 +44,7 @@ function DashBoard() {
 
   return (
     <React.StrictMode>
-      <div className="h-screen w-screen bg-white flex justify-center item-center w-screen dashboard">
+      <div className="h-screen w-screen bg-white justify-center item-center w-screen dashboard flex hidden md:flex">
         <div className="w-11/12 h-full bg-blue-300 rounded-xl ">
           <div className="exit mr-5 mt-2  float-right">
             <p
@@ -131,6 +131,122 @@ function DashBoard() {
                 ))}
               </Pie>
             </PieChart>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen justify-center item-center w-screen dashboard flex flex-col hidden">
+        <div className=" h-full bg-blue-300 rounded-xl ">
+          <div className="exit mr-5 mt-2  float-right">
+            <p
+              className="btn text-white bg-red-400 px-2 cursor-pointer"
+              onClick={closerFunc}
+            >
+              X
+            </p>
+          </div>
+          <div className="title ">
+            <p className="text-center font-bold text-3xl">Dashboard Page</p>
+            <p className="text-center text-sm text-gray-500">
+              Welcome , Kiddo !
+            </p>
+          </div>
+          <div className="flex flex-col justify-evenly mt-5 w-full">
+            <div className="box-2 h-52 bg-white w-screen  rounded-lg">
+              <div className="p-5">
+                <p className="title text-gray-300">No Of Quiz Attended</p>
+                <p className="text-9xl font-bold float-right">{changer.tot}</p>
+              </div>
+            </div>
+            <div className="box-1 h-52 w-screen  rounded-lg bg-white">
+              <div className="p-5 leading-9">
+                <p className="text-md text-gray-300 ">No Of Correct Answers</p>
+                <p className="text-9xl float-right font-bold text-green-400">
+                  {changer.correct}
+                </p>
+                {/* <div className="flex justify-between pt-3">
+                  <div className="percentage">
+                    <PieChart width={300} height={200}>
+                      <Pie
+                        dataKey="value"
+                        startAngle={180}
+                        endAngle={0}
+                        data={data}
+                        cx={cx}
+                        cy={cy}
+                        innerRadius={iR}
+                        outerRadius={oR}
+                        fill="#8884d8"
+                        stroke="none"
+                      >
+                        {data.map((entry, index) => (
+                          <Cell key={`cell-${index}`} fill={entry.color} />
+                        ))}
+                      </Pie>
+                      {needle(value1, data, cx, cy, iR, oR, "#d0d000")}
+                    </PieChart>
+                  </div>
+                </div> */}
+              </div>
+            </div>
+
+            <div className="box-3 h-52 bg-white w-screen rounded-lg">
+              <div className="p-5">
+                <p className="title text-gray-300">No Of Incorrect Answers</p>
+                <p className="text-9xl font-bold float-right text-red-400">
+                  {changer.tot * 10 - changer.correct}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center ">
+            <PieChart width={400} height={400}>
+              <Pie
+                dataKey="value"
+                startAngle={180}
+                endAngle={0}
+                data={data}
+                cx="50%"
+                cy="50%"
+                outerRadius={150}
+                fill="#8884d8"
+                labelLine={false}
+                label={renderCustomizedLabel}
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                    // key={names[index]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen w-screen justify-center items-center dashboard flex flex-col md:hidden">
+        <p
+          className="text-white bg-red-400 px-2 mt-1 rounded-md"
+          onClick={closerFunc}
+        >
+          Close
+        </p>
+        <div className="tit text-center mb-5">
+          <p className="text-lg text-white font-bold">DashBoard Page</p>
+          <p className="text-xs text-gray-300">Welcome,Kiddo!</p>
+        </div>
+        <div className="boxes">
+          <div className="box1 h-44 bg-white w-64 rounded-lg mb-3 p-4">
+            <p className="text-md text-gray-400">No Of Quiz Attended</p>
+            <p className="text-9xl float-right font-bold">{changer.tot}</p>
+          </div>
+          <div className="box1 h-44 bg-white w-64 rounded-lg mb-3 p-4">
+            <p className="text-md text-gray-400">No Of Correct Answers</p>
+            <p className="text-9xl float-right font-bold text-green-500">{changer.correct}</p>
+          </div>
+          <div className="box1 h-44 bg-white w-64 rounded-lg mb-3 p-4">
+            <p className="text-md text-gray-400">No Of Incorrect Answers</p>
+            <p className="text-9xl float-right font-bold text-red-500">{changer.tot*10-changer.correct}</p>
           </div>
         </div>
       </div>
